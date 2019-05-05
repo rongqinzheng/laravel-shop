d2186f089eecacc30bf182b6b91ddfe61e402798
R <465464547@qq.com>
2019-05-05 15:07:43 +0800

    首页

---
+++ bootstrap/helpers.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rong
+ * Date: 2019/5/5
+ * Time: 9:15 AM
+ */
+
+class Helpers
+{
+
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rong
+ * Date: 2019/5/5
+ * Time: 2:30 PM
+ */
