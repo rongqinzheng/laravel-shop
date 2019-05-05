e1d1f2c3faf7c2e885de4376e5282028f36f621f
R <465464547@qq.com>
2019-05-05 15:49:14 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rong
- * Date: 2019/5/5
- * Time: 9:15 AM
- */
-
-class Helpers
+function route_class()
 {
-
+    return str_replace('.', '-', Route::currentRouteName());
 }
